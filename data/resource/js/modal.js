e04adac2122c538c2f709a07a9f4669ef2b12936
chamexdan <61707343+chamexdan@users.noreply.github.com>
2024-09-02 21:38:17 -0400

    Add files via upload

---
+++ data/resource/js/modal.js
@@ -0,0 +1,71 @@
+const open = document.getElementById('openmodal');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('closemodal');
+
+open.addEventListener('click', () => {
+  modal_container.classList.add('show');  
+});
+
+close.addEventListener('click', () => {
+  modal_container.classList.remove('show');
+});
+
+const open2 = document.getElementById('openmodal2');
+const modal_container2 = document.getElementById('modal_container2');
+const close2 = document.getElementById('closemodal2');
+
+open2.addEventListener('click', () => {
+  modal_container2.classList.add('show');  
+});
+
+close2.addEventListener('click', () => {
+  modal_container2.classList.remove('show');
+});
+
+const open3 = document.getElementById('openmodal3');
+const modal_container3 = document.getElementById('modal_container3');
+const close3 = document.getElementById('closemodal3');
+
+open3.addEventListener('click', () => {
+  modal_container3.classList.add('show');  
+});
+
+close3.addEventListener('click', () => {
+  modal_container3.classList.remove('show');
+});
+
+const open4 = document.getElementById('openmodal4');
+const modal_container4 = document.getElementById('modal_container4');
+const close4 = document.getElementById('closemodal4');
+
+open4.addEventListener('click', () => {
+  modal_container4.classList.add('show');  
+});
+
+close4.addEventListener('click', () => {
+  modal_container4.classList.remove('show');
+});
+
+const open5 = document.getElementById('openmodal5');
+const modal_container5 = document.getElementById('modal_container5');
+const close5 = document.getElementById('closemodal5');
+
+open5.addEventListener('click', () => {
+  modal_container5.classList.add('show');  
+});
+
+close5.addEventListener('click', () => {
+  modal_container5.classList.remove('show');
+});
+
+const open6 = document.getElementById('openmodal6');
+const modal_container6 = document.getElementById('modal_container6');
+const close6 = document.getElementById('closemodal6');
+
+open6.addEventListener('click', () => {
+  modal_container6.classList.add('show');  
+});
+
+close6.addEventListener('click', () => {
+  modal_container6.classList.remove('show');
+});
